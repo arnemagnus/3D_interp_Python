18151ccc684434679581f0f160a933f51ea2b8b2
Arne Magnus <amtloken@online.no>
2018-02-07 14:45:14 +0100

    Updated gitignore

--- cytricubic/setup.py
+++ cytricubic/setup.py
@@ -1,12 +1,4 @@
 from distutils.core import setup
-#from distutils.extension import Extension
-#from Cython.Distutils import build_ext
 from Cython.Build import cythonize
-
-import numpy as np
-
-#ext_modules=[Extension("shrubbery",["cy_shrub.pyx"])]
-
-#setup(name="MyProject",cmdclass={"build_ext":build_ext},ext_modules=ext_modules)
 
 setup(name="shrubbery",ext_modules =cythonize("src/cytricubic.pyx"))
