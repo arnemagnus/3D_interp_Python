975cd15e813e7000c1960f924a8e44f2d051bd36
Arne Magnus <amtloken@online.no>
2018-02-07 14:46:52 +0100

    Removed redundant part of setup file

--- cytrilinear/setup.py
+++ cytrilinear/setup.py
@@ -1,10 +1,4 @@
 from distutils.core import setup
-#from distutils.extension import Extension
-#from Cython.Distutils import build_ext
 from Cython.Build import cythonize
-
-#ext_modules=[Extension("shrubbery",["cy_shrub.pyx"])]
-
-#setup(name="MyProject",cmdclass={"build_ext":build_ext},ext_modules=ext_modules)
 
 setup(name="shrubbery",ext_modules =cythonize("src/cytrilinear.pyx"))
